ac96601064b99202fd8ff151fa68b0fb0a2714f5
Vlada Ogai <vladamun0663@gmail.com>
2024-03-10 19:38:49 +0300

    правки по замечаниям

--- User.java
+++ User.java
@@ -1,33 +1,64 @@
+package ru.ogai.java.basic.homeworks.homework4;
+
 public class User {
     private String lastName;
-    private String firstName;
-    private String middleName;
-    private int birthYear;
+    private String name;
+    private String parentName;
+    private int birthdayYear;
     private String email;
 
-    public User(String lastName, String firstName, String middleName, int birthYear, String email) {
+    public User(String lastName, String name, String parentName, int birthdayYear, String email) {
         this.lastName = lastName;
-        this.firstName = firstName;
-        this.middleName = middleName;
-        this.birthYear = birthYear;
+        this.name = name;
+        this.parentName = parentName;
+        this.birthdayYear = birthdayYear;
         this.email = email;
+
     }
 
-
-    public void printUserInfo() {
-
-        System.out.println("ФИО: " + lastName + " " + firstName + " " + middleName);
-    }
-
-    public int getBirthYear() {
-
-        return birthYear;
+    public int getBirthdayYear() {
+        return birthdayYear;
     }
 
     public String getEmail() {
-
         return email;
     }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getParentName() {
+        return parentName;
+    }
+
+    public void setBirthdayYear(int birthdayYear) {
+        this.birthdayYear = birthdayYear;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setParentName(String parentName) {
+        this.parentName = parentName;
+    }
+
+    public void info() {
+        System.out.printf("ФИО: %s %s %s\nГод рождения: %d\nemail: %s\n", lastName, name, parentName, birthdayYear, email);
+    }
+
+    
 }
-
-
